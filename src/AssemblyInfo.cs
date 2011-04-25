2221e3d35fe000056cf661697366289d675b952f
Maik Greubel <greubel@nkey.de>
2011-04-25 11:18:38 +0200

    - Add version to assembly

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -4,4 +4,4 @@
 [assembly: AssemblyDescription("Diffie-Hellman implementation for .NET")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("../../diffiehellman.snk")]
-
+[assembly: AssemblyVersion("1.0.0.0")]
